03317741dd637459f15e938000a9aa1127e55b4b
andrei_labadaev <andrei-labadaev@mail.ru>
2021-11-15 18:25:10 +0300

    uppdate 18.25

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,53 @@
+//= components/jquery.magnific-popup.js
+
+
+
+$('.popup-content').magnificPopup({
+    type: 'inline'
+});
+
 const menuBurger = document.querySelector('.menu__burger');
+
+const tabsBtn = document.querySelectorAll('.login-tab');
+const tabsItem = document.querySelectorAll('.login-list');
+
+tabsBtn.forEach((item) => {
+    item.addEventListener('click', function () {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+
+        tabsBtn.forEach(function (item) {
+            item.classList.remove('login-active')
+        })
+
+        tabsItem.forEach(function (item) {
+            item.classList.remove('login-list-active')
+        })
+        currentBtn.classList.add('login-active');
+        currentTab.classList.add('login-list-active');
+    });
+});
+
+const formSelectInfo = document.querySelectorAll('.form__select-info');
+
+
+formSelectInfo.forEach((item) => {
+    item.addEventListener('click', function () {
+        formSelectInfo.forEach(function (item) {
+            item.classList.remove('select-active')
+
+        })
+
+        item.classList.add('select-active');
+    });
+});
+
 
 menuBurger.addEventListener('click', () => {
     const headerInner = document.querySelector('.header__inner');
     menuBurger.classList.toggle('burger__active');
     headerInner.classList.toggle('header__active');
-    document.querySelector('body').classList.toggle('look');
 })
 
 const mainBurger = document.querySelector('.main__burger');
